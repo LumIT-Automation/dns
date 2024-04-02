9e5b880405683d9aae261026f74e6724d1e10989
mik <m.sartori@lumit.it>
2024-04-02 16:38:03 +0200

    fix git commit in pkg info

--- CONTAINER-DEBIAN-PKG/DEBIAN-PKG/make-release.sh
+++ CONTAINER-DEBIAN-PKG/DEBIAN-PKG/make-release.sh
@@ -82,6 +82,7 @@ function System_definitions()
         exit 1
     fi
 
+    git config --global --add safe.directory $(cd .. && pwd)
     currentGitCommit=$(git log --pretty=oneline | head -1 | awk '{print $1}')
 
     projectName="automation-interface-dns_${debPackageRelease}_all"
