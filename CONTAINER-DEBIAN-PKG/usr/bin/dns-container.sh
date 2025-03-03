f6e0c62c57b9be329bf4d44417f484b5a9798f4e
mik <m.sartori@lumit.it>
2025-03-03 11:54:07 +0100

    check if the podman network is up before starting the container

--- CONTAINER-DEBIAN-PKG/usr/bin/dns-container.sh
+++ CONTAINER-DEBIAN-PKG/usr/bin/dns-container.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
 function start() {
+    # Check if it's up the podman internal interface (the gw of the containers), otherwise wait for 10 seconds (3 attempts).
+    gwIp=$(podman network inspect podman --format  "{{range .Subnets}}{{.Gateway}}{{end}}")
+    for _ in $(seq 1 3); do
+        if ping -c 1 -w 1 $gwIp; then
+            break
+        else
+            echo "Interface $gwIp still down."
+        fi
+        sleep 10
+    done
+
     podman start dns
 }
 
